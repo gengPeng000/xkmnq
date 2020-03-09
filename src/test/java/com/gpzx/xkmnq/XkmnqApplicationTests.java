37e90ad7d2ce487ee966cadd52b2e61dd4d8682f
gengPeng000 <302680628@qq.com>
2020-03-09 21:47:36 +0800

    笑傲江湖模拟器第一次提交

---
+++ src/test/java/com/gpzx/xkmnq/XkmnqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpzx.xkmnq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XkmnqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
